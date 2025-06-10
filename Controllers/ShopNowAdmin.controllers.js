5e4cb83093f67ff0970bfb841288c52facbc1e84
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-06-10 22:20:17 +0530

    admin product added changes

--- Controllers/ShopNowAdmin.controllers.js
+++ Controllers/ShopNowAdmin.controllers.js
@@ -142,8 +142,8 @@ export const AddProducts = async(req,res,next) =>{
 
                 //Remove files from local
                 try {
-                    await fs.access(filePath); // Check if file exists
-                    await fs.rm(filePath);     // Then remove it
+                     await fs.rm(filePath, { force: true }); // 🔐 Safely delete even if missing
+                    
                 } catch (err) {
                         console.warn('File not found or could not be deleted:', err.message);
                 }
